7f231a7de20cb2ba15d94d234b362902c153ddbe
vgnilrem <velasqmg@gmail.com>
2025-04-14 18:21:13 -0700

    math round follows rounding rules automatically based on the number, math floor rounds down to a whole number and math ceil rounds up to the next whole number

---
+++ numbermethods.js
@@ -0,0 +1,21 @@
+// w a function make prompt the user to enter a decimal number --> round it using Math.round() and display result
+
+function roundDecimal() {
+    let userInput = prompt("Enter a decimal number:");
+
+    let decimalNumber = parseFloat(userInput);
+
+    let roundedNumber = Math.round(decimalNumberNumber);
+
+    console.log("The rounded number it:" + roundedNumber);
+}
+
+// w a function take an array of decimal numbers and create a new array with all values rounded down using Math.floor()
+
+let decimalNumbers = []
+Math.floor(decimalNumbers)
+
+// online store rounds all prices up to the nearest WHOLE number. Convert an array of prices to the nearest whole number using Math.ceil() 
+
+let onlineStorePrice = [10.5, 20.4, 32.5, 16.75];
+console.log(Math.ceil(onlineStorePrice));
