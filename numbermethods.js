adf8a455f839488280d0e563860a937f4074f98c
vgnilrem <velasqmg@gmail.com>
2025-04-14 19:03:07 -0700

    NaN checks if the value is not a number, you get true if it's not a number, false if it is a number, parseFloat will take your string and convert to a  decimal, while  parseInt will takes a string into a integer

--- numbermethods.js
+++ numbermethods.js
@@ -5,17 +5,39 @@ function roundDecimal() {
 
     let decimalNumber = parseFloat(userInput);
 
-    let roundedNumber = Math.round(decimalNumberNumber);
+    let roundedNumber = Math.round(decimalNumber);
 
-    console.log("The rounded number it:" + roundedNumber);
+    console.log("The rounded number is:" + roundedNumber);
 }
 
 // w a function take an array of decimal numbers and create a new array with all values rounded down using Math.floor()
 
-let decimalNumbers = []
-Math.floor(decimalNumbers)
+let decimalNumbers = [12.7, 3.4, 9.8];
+let roundedDownNumbers = decimalNumbers.map(num => Math.floor(num)); // map makes it loop through the array and apply math floor to each value inside
+console.log(roundedDownNumbers); //now print to display!
 
 // online store rounds all prices up to the nearest WHOLE number. Convert an array of prices to the nearest whole number using Math.ceil() 
 
 let onlineStorePrice = [10.5, 20.4, 32.5, 16.75];
-console.log(Math.ceil(onlineStorePrice));
+let roundedUpPrices = onlineStorePrice.map(price => Math.ceil(price)); // remember you can use map to call the math ceil to the whole array
+console.log(roundedUpPrices);
+
+
+// Number Methods --> create a function that take a number and checks whether it is a perfect square
+
+function aPerfectSquare(num) {
+    return Math.sqrt(num) % 1 === 0;
+}
+
+console.log(aPerfectSquare(16)); // true
+
+// given an array of numbers, find the smallest number using Math.min()
+
+let arrayOfNumbers = [25, 26, 28, 30];
+let smallestnNumber= Math.min(arrayofNumbers);
+console.log(smallestNumber);
+
+// given an array of numbers, find the largest number using Math.max()
+let newNumbersArray = [25, 26, 28, 30];
+let largestNumber = Math.max(newNumbersArray);
+console.log(largestNumber);
